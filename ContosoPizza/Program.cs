1caea2a0b37348570638cf6656a1726efcd099a9
silvadaniell <djs@ic.ufal.br>
2024-08-20 16:33:47 -0300

    Persistir e recuperar dados relacionais com o Entity Framework Core

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,4 +1,5 @@
 using ContosoPizza.Services;
+using ContosoPizza.Data;
 // Additional using declarations
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
-
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 // Add the PromotionsContext
 
 builder.Services.AddScoped<PizzaService>();
