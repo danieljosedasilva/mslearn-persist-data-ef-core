e29aa646cb5428a5738e706e08fa209e67964554
silvadaniell <djs@ic.ufal.br>
2024-08-21 08:39:02 -0300

    feat(PizzaService): implementação do CRUD e adicionando dependências

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,4 +1,5 @@
 using ContosoPizza.Services;
+using ContosoPizza.Data;
 // Additional using declarations
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
-
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 // Add the PromotionsContext
 
 builder.Services.AddScoped<PizzaService>();
@@ -26,7 +27,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
 
 app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
